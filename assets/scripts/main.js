223f03d91e0e27926462cb4162bf85a2ed1596f0
samantha moss <samantha.moss87@aol.com>
2024-02-20 19:39:25 +0000

    Removed management popup and make it static.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -32,19 +32,6 @@ function scrollToTop() {
 }
 
 
-// Functions for management page in the nav
-const aboutLink = document.getElementById("aboutLink")
-const managementArea = document.getElementById("managementArea")
-const managementLink = document.getElementById("managementLink")
-
-aboutLink.addEventListener('mouseenter', function() {
-    managementArea.classList.add('active')
-})
-managementLink.addEventListener('click', function(){
-    managementArea.classList.remove("active")
-})
-
-
 // Logic for the languages
 const languageBtn = document.getElementById("changeLangBtn")
 const languagePopup = document.getElementById("langOptions")
